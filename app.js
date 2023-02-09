1c6766a7890841a5db751c9f3b3e8231cc7db214
unknown <sxcueto@gmail.com>
2023-02-08 20:48:29 -0500

    push to git

---
+++ app.js
@@ -0,0 +1,20 @@
+function addTwo (x,y){
+    console.log (x+y)
+    }
+    addTwo(10,20);
+// Create a function that returns the difference of two numbers that are arguments. Then console.log the function with the variables from step two as your two arguments.
+function subTwo (a,b){
+    console.log (a-b)
+}
+subTwo(10,20);
+// Create a function that returns two arguments multiplied by each other. Then console.log the function with the variables from step two as your two arguments.
+function mulTwo (a,b){
+    console.log (a*b)
+}
+mulTwo (10,20)
+// Create a function that returns divides the first argument by the second argument and returns the new value. Then console.log the function with the variables from step two as your two arguments.
+
+function divTwo (a,b){
+    console.log (b/a)
+}
+divTwo (10,20)
